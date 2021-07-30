98e54fa272631a8ceafa851ee5ae64e8427958d0
Austin Shelby <73789390+AustinShelby@users.noreply.github.com>
2021-07-30 17:17:23 +0200

    locales

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en", "de", "catchall"],
+    defaultLocale: "catchall",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/catchall",
+        destination: "/en",
+        locale: false,
+        permanent: false,
+      },
+      {
+        source: "/catchall/:slug*",
+        destination: "/en/:slug*",
+        locale: false,
+        permanent: false,
+      },
+    ];
+  },
+};
